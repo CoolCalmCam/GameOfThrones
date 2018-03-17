bd9ebdc4f702c231e7b3c43858ce0e8876c7860a
CoolCalmCam <kicker155@aol.com>
2018-03-16 21:12:58 -0500

    Add files via upload

--- DoubleSpeed.java
+++ DoubleSpeed.java
@@ -27,25 +27,25 @@ public class DoubleSpeed implements Observer, FactoryInterface {
 	}
 
 	public void goNorth(Point p) {
-		if (p.y > 0 && oceanMap.isOccupied(p.x, p.y - 1)) {
+		if (p.y > 0 && !oceanMap.isOccupied(p.x, p.y - 1)) {
 			p.y -= 2;
 		}
 	}
 
 	public void goEast(Point p) {
-		if (p.x < 9 && oceanMap.isOccupied(p.x + 1, p.y)) {
+		if (p.x < 9 && !oceanMap.isOccupied(p.x + 1, p.y)) {
 			p.x += 2;
 		}
 	}
 
 	public void goWest(Point p) {
-		if (p.x > 0 && oceanMap.isOccupied(p.x - 1, p.y)) {
+		if (p.x > 0 && !oceanMap.isOccupied(p.x - 1, p.y)) {
 			p.x -= 2;
 		}
 	}
 
 	public void goSouth(Point p) {
-		if (p.y < 9 && oceanMap.isOccupied(p.x, p.y + 1)) {
+		if (p.y < 9 && !oceanMap.isOccupied(p.x, p.y + 1)) {
 			p.y += 2;
 		}
 	}
@@ -54,25 +54,25 @@ public class DoubleSpeed implements Observer, FactoryInterface {
 		if (arg0 instanceof Ship) {
 			shipLocation = ((Ship) arg0).getShipLocation();
 			if (shipLocation.x < currentLocation.x) {
-				if (currentLocation.x > 0 && oceanMap.isOccupied(currentLocation.x - 2, currentLocation.y)) {
+				if (currentLocation.x > 0 && !oceanMap.isOccupied(currentLocation.x - 2, currentLocation.y)) {
 					goWest(currentLocation);
 				}
 			}
 			if (shipLocation.y > currentLocation.y) {
 				if (currentLocation.y < oceanMap.getDimensions()
-						&& oceanMap.isOccupied(currentLocation.x, currentLocation.y + 2)) {
+						&& !oceanMap.isOccupied(currentLocation.x, currentLocation.y + 2)) {
 					goSouth(currentLocation);
 				}
 
 			}
 			if (shipLocation.y < currentLocation.y) {
-				if (currentLocation.y > 0 && oceanMap.isOccupied(currentLocation.x, currentLocation.y - 2)) {
+				if (currentLocation.y > 0 && !oceanMap.isOccupied(currentLocation.x, currentLocation.y - 2)) {
 					goNorth(currentLocation);
 				}
 			}
 			if (shipLocation.x > currentLocation.x) {
 				if (currentLocation.x < oceanMap.getDimensions()
-						&& oceanMap.isOccupied(currentLocation.x + 2, currentLocation.y)) {
+						&& !oceanMap.isOccupied(currentLocation.x + 2, currentLocation.y)) {
 					goEast(currentLocation);
 				}
 			}
